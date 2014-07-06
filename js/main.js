d4eb2623354696e86c28a3b439d82bf48b4f82ed
Gary Zhou <zzgary92@gmail.com>
2014-07-06 13:05:33 +0800

    update for web and mobile

--- js/main.js
+++ js/main.js
@@ -5,10 +5,15 @@ minutes=1000*60;
 hours=minutes*60;
 days=hours*24;
 years=days*365;
-storage = chrome.storage.local;
+if(typeof chrome.storage !="undefined"){
+  storage = chrome.storage.local;
+}else{
+  storage = $.localStorage;
+}
 $(document).ready(function(){
     showquotes();
     setTimeout(function() {
+      if(typeof chrome.storage !="undefined"){
         storage.get("currentpromise", function(data) {
             if(data.currentpromise){
                 $('div.loading').fadeOut(500).siblings('div.cupromise').fadeIn(800);
@@ -17,8 +22,17 @@ $(document).ready(function(){
                 $('div.loading').fadeOut(500).siblings('div.addpromise').fadeIn(800);
             }
         });
+      }else{
+        var data = storage.get('currentpromise');
+        if(data){
+            $('div.loading').fadeOut(500).siblings('div.cupromise').fadeIn(800);
+            showcur(data);
+        }else{
+            $('div.loading').fadeOut(500).siblings('div.addpromise').fadeIn(800);
+        }
+      }
     }, 3000);
-    // TODO: optimize the date picker
+    // DONE: optimize the date picker
     $('#datetimepicker').datetimepicker({
       format: 'yyyy-MM-dd hh:mm:ss',
       language: 'pt-BR'
@@ -31,13 +45,11 @@ $(document).ready(function(){
         var cupromise = {};
         cupromise.content = $('textarea').val();
         cupromise.addtime = new Date();
-        cupromise.deadline = $('input#theddl').val().replace(" ","T");
+        cupromise.deadline = $('input#theddl').val().replace(" ","T")+'+0800';
         if(cupromise.deadline == ""){showpop('invalid date');return};
-        if(Date.parse(cupromise.deadline)-Date.parse(Date()) >= (1000*60*60*24*5)){showpop('Your deadline should be in 5 days from now on.');return};
-        if(Date.parse(cupromise.deadline)-Date.parse(Date()) <= 0){showpop('Your deadline must be later than now.');return};
-        storage.set({"currentpromise":cupromise}, function() {
-            console.log('saved to currentpromise.')
-        });
+        if(Date.parse(cupromise.deadline)-Date.parse(new Date()) >= (1000*60*60*24*5)){showpop('Your deadline should be in 5 days from now on.');return};
+        if(Date.parse(cupromise.deadline)-Date.parse(new Date()) <= 0){showpop('Your deadline must be later than now.');return};
+        storage.set({"currentpromise":cupromise});
         $('div.addpromise').fadeOut(800).siblings('div.cupromise').fadeIn(800);
         showcur(cupromise);
     });
@@ -45,19 +57,32 @@ $(document).ready(function(){
     $('div.showpromise').on('click','input',function(){
       var mark = ($(this).prop('checked'))?1:0;
       var imark = $('input').index($(this));
-      storage.get("marks", function(data) {
-        if(Object.keys(data).length==0){
+      if(typeof chrome.storage !="undefined"){
+        storage.get("marks", function(data) {
+          if(Object.keys(data).length==0){
+            data[imark] = mark;
+            storage.set({"marks":data});
+          }else{
+            // console.log(data);
+            data['marks'][imark] = mark;
+            storage.set({"marks":data.marks});
+          }
+        })
+      }else{
+        var data = storage.get("marks");
+        if(data==null){
           data[imark] = mark;
           storage.set({"marks":data});
         }else{
           // console.log(data);
-          data['marks'][imark] = mark;
-          storage.set({"marks":data.marks});
+          data[imark] = mark;
+          storage.set({"marks":data});
         }
-      })
+      }
     });
     // mark done
     $('div.mdone').on('click',function(){
+      if(typeof chrome.storage !="undefined"){
         storage.get("currentpromise", function(data) {
             kk = data.currentpromise;
             if(Date.parse(kk.deadline)-Date.parse(Date())>=0){
@@ -79,9 +104,26 @@ $(document).ready(function(){
                 });
             });
         });
-        storage.remove("marks");
+      }else{
+        var kk = storage.get("currentpromise");
+        if(Date.parse(kk.deadline)-Date.parse(Date())>=0){
+            kk.status = "done";
+        }else{
+            kk.status = "delay";
+        }
+        kk.closetime = Date();
+        var his = storage.get("historypromise");
+        if(his == null){his = {}};
+        var i = Object.keys(his).length;
+        his[i] = kk;
+        storage.set({"historypromise":his});
+        storage.remove("currentpromise");
+        $('div.cupromise').fadeOut(500).siblings('div.addpromise').fadeIn(800);
+      }
+      storage.remove("marks");
     });
     $('div.sorry').on('click',function(){
+      if(typeof chrome.storage !="undefined"){
         storage.get("currentpromise", function(data) {
             var kk = data.currentpromise;
             kk.status = "fail";
@@ -99,6 +141,17 @@ $(document).ready(function(){
                 });
             });
         });
+      }else{
+        var kk = storage.get("currentpromise");
+        kk.status = "fail";
+        kk.closetime = Date();
+        var his = storage.get("historypromise");
+        if(his == null){his = []};
+        his.push(kk);
+        storage.set({"historypromise":his});
+        storage.remove("currentpromise");
+        $('div.cupromise').fadeOut(500).siblings('div.addpromise').fadeIn(800);
+      }
         storage.remove("marks");
     });
     $('div.cupromise').on('click','div.right,div.left',function(){
@@ -106,6 +159,7 @@ $(document).ready(function(){
         showhis();
     });
     $('div.oldpromise').on('click','div.right,div.left',function(){
+      if(typeof chrome.storage !="undefined"){
         storage.get("currentpromise", function(data) {
             if(data.currentpromise){
                 $('div.oldpromise').fadeOut(500).siblings('div.cupromise').fadeIn(800);
@@ -113,8 +167,17 @@ $(document).ready(function(){
                 $('div.oldpromise').fadeOut(500).siblings('div.addpromise').fadeIn(800);
             }
         });
+      }else{
+        var data = storage.get("currentpromise");
+        if(data){
+            $('div.oldpromise').fadeOut(500).siblings('div.cupromise').fadeIn(800);
+        }else{
+            $('div.oldpromise').fadeOut(500).siblings('div.addpromise').fadeIn(800);
+        }
+      }
     });
     $('div.addpromise').on('click','div.right,div.left',function(){
+      if(typeof chrome.storage !="undefined"){
         storage.get("currentpromise", function(data) {
             if(data.currentpromise){
                 $('div.addpromise').fadeOut(500).siblings('div.cupromise').fadeIn(800);
@@ -123,14 +186,42 @@ $(document).ready(function(){
                 showhis();
             }
         });
+      }else{
+        var data = storage.get("currentpromise");
+        if(data){
+            $('div.addpromise').fadeOut(500).siblings('div.cupromise').fadeIn(800);
+        }else{
+            $('div.addpromise').fadeOut(500).siblings('div.oldpromise').fadeIn(800);
+            showhis();
+        }
+      }
     });
     $('div.clear').on('click',function(){
+      if(typeof chrome.storage !="undefined"){
         storage.remove("historypromise", function() {
             console.log('Cleared all histories.')
             showhis();
         });
+      }else{
+        storage.remove("historypromise");
+        showhis();
+      }
     });
+    //TODO: add review for history item.
+    $('div.histories').on('click','ul.items li',function(){
+        $('div.review').show();
+        if(typeof chrome.storage !="undefined"){
+          storage.get("historypromise", function(data) {
+              var his = data.historypromise;
+
+          });
+        }else{
+          var data = storage.get("historypromise");
+        }
+    });
+    // update the ddl time information
     setInterval(function() {
+      if(typeof chrome.storage !="undefined"){
         storage.get("currentpromise", function(data) {
             if(data.currentpromise){
                 showcur(data.currentpromise);
@@ -138,6 +229,14 @@ $(document).ready(function(){
                 return
             }
         });
+      }else{
+        var data = storage.get("currentpromise");
+        if(data){
+            showcur(data);
+        }else{
+            return
+        }
+      }
     },60000);
 });
 function showcur(kk){
@@ -150,7 +249,7 @@ function showcur(kk){
       $('div.showpromise').html(kk.content.replace(/\n/g,"\<br\>"));
     }
     $('div.ddl span.time').text(kk.deadline);
-    var remaintime =Date.parse(kk.deadline)-Date.parse(Date());
+    var remaintime =Date.parse(kk.deadline)-Date.parse(new Date());
     if(remaintime>3*days){
         flag = "long";
     }else if(remaintime>2*days){
@@ -173,32 +272,58 @@ function showthis(remaintime,flag){
 }
 // show marks after load all data
 function showmarks(){
+  if(typeof chrome.storage !="undefined"){
     storage.get("marks", function(data) {
       $.each(data.marks,function(i,v){
           $('input').eq(parseInt(i)).prop('checked',((v==1)? true:false));
       });
     })
+  }else{
+    var data = storage.get("marks");
+    $.each(data,function(i,v){
+        $('input').eq(parseInt(i)).prop('checked',((v==1)? true:false));
+    });
+  }
 }
 function showhis(){
     $('ul.items').empty();
-    storage.get("historypromise", function(data) {
-        var his = data.historypromise;
-        var savedtime = 0;
-        if(his != null){
-            $.each(his,function(index,value){
-              // remove the 'MD:' for summary
-                var summ = value.content.split("\n")[0];
-                if(value.content.indexOf("MD:")==0){
-                    summ = value.content.split("\n")[1];
-                }
-                $('ul.items').append("<li class="+value.status+"><span>"+value.closetime.split("GMT")[0]+"</span><span>"+value.status+"</span><div class="+value.status+"></div><div class=summary>"+summ+"</div></li>");
-                if(value.status == 'done' || value.status == 'delay'){
-                    savedtime = savedtime +  Date.parse(value.deadline)-Date.parse(value.closetime);
-                }
-            });
-        }
-        $('div.savetime').find('span').text(Math.floor(savedtime/(1000*3600*24))+"days");
-    });
+    if(typeof chrome.storage !="undefined"){
+      storage.get("historypromise", function(data) {
+          var his = data.historypromise;
+          var savedtime = 0;
+          if(his != null){
+              $.each(his,function(index,value){
+                // remove the 'MD:' for summary
+                  var summ = value.content.split("\n")[0];
+                  if(value.content.indexOf("MD:")==0){
+                      summ = value.content.split("\n")[1];
+                  }
+                  $('ul.items').append("<li class="+value.status+"><span>"+value.closetime.split("GMT")[0]+"</span><span>"+value.status+"</span><div class="+value.status+"></div><div class=summary>"+summ+"</div></li>");
+                  if(value.status == 'done' || value.status == 'delay'){
+                      savedtime = savedtime +  Date.parse(value.deadline)-Date.parse(value.closetime);
+                  }
+              });
+          }
+          $('div.savetime').find('span').text(Math.floor(savedtime/(1000*3600*24))+"days");
+      });
+    }else{
+      var his = storage.get("historypromise");
+      var savedtime = 0;
+      if(his != null){
+          $.each(his,function(index,value){
+            // remove the 'MD:' for summary
+              var summ = value.content.split("\n")[0];
+              if(value.content.indexOf("MD:")==0){
+                  summ = value.content.split("\n")[1];
+              }
+              $('ul.items').append("<li class="+value.status+"><span>"+value.closetime.split("GMT")[0]+"</span><span>"+value.status+"</span><div class="+value.status+"></div><div class=summary>"+summ+"</div></li>");
+              if(value.status == 'done' || value.status == 'delay'){
+                  savedtime = savedtime +  Date.parse(value.deadline)-Date.parse(value.closetime);
+              }
+          });
+      }
+      $('div.savetime').find('span').text(Math.floor(savedtime/(1000*3600*24))+"days");
+    }
 }
 
 function showpop(mes){
@@ -220,6 +345,7 @@ function showpop(mes){
     setTimeout($.unblockUI, 2000);
 }
 function showquotes(){
+  if(typeof chrome.storage !="undefined"){
     storage.get("dailyquote", function(data) {
         if(data.dailyquote && (Date.parse(Date())-Date.parse(data.dailyquote.gettime))<days){
             var quote = data.dailyquote;
@@ -237,4 +363,19 @@ function showquotes(){
             })
         }
     });
+  }else{
+    var quote = storage.get("dailyquote");
+    if(quote && (Date.parse(Date())-Date.parse(quote.gettime))<days){
+        $('span.quote').html(quote.contents.quote);
+        $('span.author').html(quote.contents.author);
+    }else{
+        $.get("http://api.theysaidso.com/qod.json",function(data){
+            quote = data;
+            quote.gettime = (new Date()).toDateString();
+            $('span.quote').html(quote.contents.quote);
+            $('span.author').html(quote.contents.author);
+            storage.set({"dailyquote":quote});
+        })
+    }
+  }
 }
